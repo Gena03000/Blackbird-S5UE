7c2e29d0ab1729888a77f2fa809777160fb7c0b0
Gena03 <162874586+Gena03000@users.noreply.github.com>
2025-08-27 13:27:05 +0200

    Update server.js
    Signed-off-by: Gena03 <162874586+Gena03000@users.noreply.github.com>

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const displayUrl = process.env.PORT
   ? 'https://merle.up.railway.app'
   : `http://localhost:${port}`;
